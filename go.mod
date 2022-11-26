f388dd8c31aeff5e2e9d5fd49a7d078370ae53d5
BigSuperBenny <794516576@qq.com>
2022-11-27 00:37:13 +0800

    feat(coreweb):init framework

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BigSuperBenny/ShirleyCore_Web
+
+go 1.19
---
+++ web/web.go
@@ -0,0 +1,51 @@
+package web
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// HandlerFunc defines the request handler
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+// Engine implement the interface of ServerHTTP
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// New is the constructor of web.engine
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+// web engine add route
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+
+	if handler, ok := engine.router[key]; !ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND:%s\n", req.URL.Path)
+	}
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run() {
+	log.Fatal(http.ListenAndServe(":9999", engine))
+}
